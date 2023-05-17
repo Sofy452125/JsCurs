d33788919aaecfcdf47e19e0ed8787fd14ff81b5
Sofy452125 <133798363+Sofy452125@users.noreply.github.com>
2023-05-17 13:11:08 +0300

    Add files via upload

---
+++ JsSofy/js/lesson1.js
@@ -0,0 +1,16 @@
+let title = 'Калькулятор Верстки'
+let screens = 'Простые, Сложные , Aдаптивные'
+let screenPrice = 5000
+let rollback = 100
+let fullPrice = 100000
+let adaptive = true
+
+
+console.log(typeof(title));
+console.log(typeof(fullPrice));
+console.log(screens.length);
+console.log(screenPrice + ' Рублей');
+console.log(fullPrice + ' Рублей');
+// console.log(screens.toLowerCase());
+console.log(screens.split() , screens.toLowerCase());
+console.log(fullPrice * (rollback/100));
